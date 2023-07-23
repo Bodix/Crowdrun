f2beb395147d8687132b95a69738ed3e5ce4aa9a
Bodix <bodix321@gmail.com>
2023-07-23 05:59:57 +0300

    Improve spawn logic, add font

--- Assets/_Game/Scripts/Player.cs
+++ Assets/_Game/Scripts/Player.cs
@@ -10,6 +10,8 @@ public class Player : MonoBehaviour
     [SerializeField]
     private int _charactersCount = 5;
     [SerializeField]
+    private float _distanceBetweenCharacters = 0.4f;
+    [SerializeField]
     private float _forwardSpeed = 3;
     [SerializeField]
     private float _directionalSpeed = 1;
@@ -20,7 +22,6 @@ public class Player : MonoBehaviour
     private Transform _crowdContainer;
 
     private readonly List<Character> _characters = new(512);
-    private const float SpiralRatio = 0.5f;
 
     private void Awake()
     {
@@ -28,34 +29,52 @@ public class Player : MonoBehaviour
 
         for (int i = 0; i < _charactersCount; i++)
         {
-            _characters.Add(Instantiate(_characterPrefab, GetSpawnPosition(i), Quaternion.identity, _crowdContainer)
+            Vector2 position = GetPhyllotaxisPosition(i, _distanceBetweenCharacters);
+            _characters.Add(Instantiate(
+                    _characterPrefab,
+                    new Vector3(position.x, transform.position.y, position.y),
+                    Quaternion.identity,
+                    _crowdContainer)
                 .Initialize(_forwardSpeed, _directionalSpeed));
-        }
-
-        foreach (Character character in _characters)
-        {
-            character.Move(Random.value);
         }
     }
 
     private void Move(float input)
     {
-        foreach (Character character in _characters)
-        {
-            character.Move(input);
-        }
+        transform.Translate(new Vector3(input * _directionalSpeed * 0.01f, 0, 0));
+    }
+
+    /// <summary>
+    /// https://www.youtube.com/watch?v=YCFt0L5KNWE
+    /// </summary>
+    public Vector2 GetPhyllotaxisPosition(int count, float scale = 0.5f, float degree = 137.5f)
+    {
+        float angle = count * (degree * Mathf.Deg2Rad);
+        float radius = scale * Mathf.Sqrt(count);
+
+        return GetCirclePosition(radius, angle);
+    }
+
+    /// <summary>
+    /// Parametric equation of the circle.
+    /// </summary>
+    public Vector2 GetCirclePosition(float radius, float angle)
+    {
+        float x = radius * Mathf.Cos(angle);
+        float y = radius * Mathf.Sin(angle);
+
+        return new Vector2(x, y);
     }
 
     /// <summary>
     /// Parametric equation of the spiral.
-    /// http://old.exponenta.ru/soft/others/stud1/main.asp#:~:text=%D0%9F%D0%B0%D1%80%D0%B0%D0%BC%D0%B5%D1%82%D1%80%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%B5%20%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5%3A,%D1%81%D0%BE%D0%B2%D0%BF%D0%B0%D0%B4%D0%B0%D0%B5%D1%82%20%D1%81%20%D1%86%D0%B5%D0%BD%D1%82%D1%80%D0%BE%D0%BC%20%D0%B2%D1%80%D0%B0%D1%89%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%BF%D1%80%D1%8F%D0%BC%D0%BE%D0%B9.
     /// </summary>
-    private Vector3 GetSpawnPosition(int index)
+    private Vector2 GetSpiralPosition(int count, float ratio)
     {
-        float t = index * SpiralRatio;
-        float x = t * Mathf.Cos(t);
-        float y = t * Mathf.Sin(t);
+        float radius = count * ratio;
+        float x = radius * Mathf.Cos(radius);
+        float y = radius * Mathf.Sin(radius);
 
-        return new Vector3(x, 0, y);
+        return new Vector2(x, y);
     }
 }
